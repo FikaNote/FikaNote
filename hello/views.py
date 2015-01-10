41544e4149d8afd91cfa026a4ea04e5e8b7ea18d
Kosuke Nagano <gm.charlie@gmail.com>
2015-01-10 23:08:20 +0900

    Modify to test

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,7 +6,9 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
