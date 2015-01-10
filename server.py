7d0d495ccb5d6599f70221395c9c1d2f1aba2524
Kosuke Nagano <gm.charlie@gmail.com>
2015-01-10 17:54:28 +0900

    add route for /

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from bottle import route, run, template
 
 @route('/hello/<name>')
-def index(name):
+def hello(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
+@route('/')
+def index():
+    return template('<b>Index</b>!')
 
 run(host='fikanote.herokuapp.com', port=80)
