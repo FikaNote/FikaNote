f610a428d9601d784bedc0ae7a3491049235d3a1
Kosuke Nagano <gm.charlie@gmail.com>
2015-01-12 23:50:54 +0900

    update js to post url to /add

--- gettingstarted/static/js/postagenda.js
+++ gettingstarted/static/js/postagenda.js
@@ -1,12 +1,21 @@
 angular.module('addAgenda', [])
-  .controller('AgendaController', ['$scope', function($scope) {
+  .controller('AgendaController', function($scope, $http) {
     $scope.agendas = [];
  
     $scope.addAgenda = function() {
       $scope.agendas.push({text:$scope.agendaText, done:false});
       $scope.agendaText = '';
+      $http.post('/add', {url:$scope.agendaText}).
+        success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+      }).
+        error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
     };
- 
+                                               
     $scope.remaining = function() {
       var count = 0;
       angular.forEach($scope.agendas, function(agenda) {
@@ -22,4 +31,4 @@ angular.module('addAgenda', [])
         if (!agenda.done) $scope.agendas.push(agenda);
       });
     };
-  }]);
+  });
