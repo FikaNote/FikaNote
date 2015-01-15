2cbb8ee33ef2006b77a9b54dd422a0938802c51a
Kosuke Nagano <gm.charlie@gmail.com>
2015-01-15 13:01:19 +0900

    modify to reference mongolab

--- hello/views.py
+++ hello/views.py
@@ -8,26 +8,21 @@ from django.template import RequestContext
 
 from .models import Greeting
 
+import pymongo
+from pymongo import ASCENDING, DESCENDING
+
+MONGODB_URI = 'mongodb://fikakou:0US3ZKxV@ds029811.mongolab.com:29811/fikanotedb' 
+
 # Create your views here.
 def index(request):
-    episodes = []
-    episodes.append({
-        'agenda':'あれとこれをそれしました' ,
-        'year' : '2015',
-        'date' : '05/Jan',
-        'title' : 'はじめてのFika',
-        'number' : 1
-        })
-    episodes.append({
-        'agenda':'hogeとfugaについて話しました' ,
-        'year' : '2015',
-        'date' : '09/Jan',
-        'title' : '開発者のこれからについて',
-        'number' : 2
-        })
+    client = pymongo.MongoClient(MONGODB_URI)
+    db = client.get_default_database()
+    fikanote = db['fikanotedb']
+    episodes = fikanote.find().sort("date", pymongo.DESCENDING)
+    client.close()
     return render(request, 'index.html', 
                   {'episodes': episodes
-                  } )
+                   } )
 
 def episode(request, number):
     persons = []
@@ -63,11 +58,15 @@ def agenda(request):
 
 def add(request):
     if request.method == 'POST':
-        content = 'not support this method'
-        return HttpResponse(content, content_type='text/plain')
+        response_data = {}
+        response_data['result'] = 'success'
+        response_data['message'] = 'success'
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
     else:
-        content = 'not support this method'
-        return HttpResponse(content, content_type='text/plain')
+        response_data = {}
+        response_data['result'] = 'failed'
+        response_data['message'] = 'not support this method'
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def db(request):
 
