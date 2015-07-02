82740d78aa63e485f1c0acad4d2549009dce8d8c
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-07-02 23:52:43 +0900

    #56 display trash box after post url.

--- app/agenda.py
+++ app/agenda.py
@@ -12,17 +12,17 @@ import datetime
 import httplib
 
 def agenda(request):
-    if request.method == 'GET': 
+    if request.method == 'GET':
         agendas = AgendaDB.objects().order_by('-date')
-        form = AgendaForm() 
-        return render(request, 'agenda.html', 
-                      {'agendas': agendas, 
+        form = AgendaForm()
+        return render(request, 'agenda.html',
+                      {'agendas': agendas,
                        'form': form
                        } )
 
-    elif request.method == 'POST': 
-        form = AgendaForm(request.POST) 
-        if form.is_valid(): 
+    elif request.method == 'POST':
+        form = AgendaForm(request.POST)
+        if form.is_valid():
             try:
                 url = form.cleaned_data['url']
                 req = urllib2.Request(url, None, headers = { 'User-Agent' : 'Mozilla/5.0' })
@@ -32,15 +32,16 @@ def agenda(request):
             except httplib.BadStatusLine as e:
                 title = "UNNAMED URL"
 
-            AgendaDB(url=url, title=title, date=datetime.datetime.utcnow()).save()
+            agendaItem = AgendaDB(url=url, title=title, date=datetime.datetime.utcnow())
+            agendaItem.save()
 
-            response = json.dumps({'status':'success', 'url':url, 'title':title})  # convert to JSON
+            response = json.dumps({'status':'success', 'url':url, 'title':title, "id":str(agendaItem.id)})  # convert to JSON
         else:
             response = json.dumps({'status':'fail'})  # convert to JSON
 
         return HttpResponse(response, content_type="application/json")
 
-    elif request.method == 'DELETE': 
+    elif request.method == 'DELETE':
         delete = QueryDict(request.body)
         delete_id = delete.get('id')
         if delete_id :
@@ -52,4 +53,3 @@ def agenda(request):
 
     else:
         raise Http404
-
