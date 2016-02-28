d5cf0482eb5abe0ba62a467eeafed57fef7630fe
Kosuke Nagano <gm.charlie@gmail.com>
2016-02-29 00:55:29 +0900

    remove deprecated functions

--- fikanote/urls.py
+++ fikanote/urls.py
@@ -5,15 +5,12 @@ import app.shownote
 import app.agenda
 import app.agendajson
 
-urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'fikanote.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-                       url(r'^$', app.views.index, name='index'),
-                       url(r'^(?P<number>\d+)/$', app.views.episode),
-                       url(r'^agenda', app.agenda.agenda),
-                       url(r'^agendajson', app.agendajson.agendajson),
-                       url(r'^add', app.views.add, name='add'),
-                       url(r'^shownote', app.shownote.shownote),
-                       url(r'^feed/', app.feed.feed)
-)
+urlpatterns = [
+    url(r'^$', app.views.index, name='index'),
+    url(r'^(?P<number>\d+)/$', app.views.episode),
+    url(r'^agenda', app.agenda.agenda),
+    url(r'^agendajson', app.agendajson.agendajson),
+    url(r'^add', app.views.add, name='add'),
+    url(r'^shownote', app.shownote.shownote),
+    url(r'^feed/', app.feed.feed)
+]
