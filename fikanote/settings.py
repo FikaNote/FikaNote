3c78152d091cdee93d858ff03832d1726312d4c9
Nagano Kosuke <gm_kou@gmail.com>
2020-12-06 11:27:50 +0900

    revert

--- fikanote/settings.py
+++ fikanote/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # Parse database configuration from $DATABASE_URL
 # Parse database configuration from $DATABASE_URL
-#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
