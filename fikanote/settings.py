884af634466e1d846d0e12f9b8975e2c5fbc9374
Kosuke Nagano <gm.charlie@gmail.com>
2020-12-06 10:09:39 +0900

    Update settings.py

--- fikanote/settings.py
+++ fikanote/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,19 +39,19 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'sass_processor',
     'app'
-)
+]
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
+MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
-)
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+]
 
 ROOT_URLCONF = 'fikanote.urls'
 
@@ -102,8 +102,7 @@ SASS_PROCESSOR_ROOT = STATICFILES_DIRS[0]
 
 TEMPLATE_LOADERS = [
     'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    'django.template.context_processors.request'
+    'django.template.loaders.app_directories.Loader'
 ]
 
 TEMPLATES = [
@@ -118,12 +117,12 @@ TEMPLATES = [
                 # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
                 # list if you haven't customized them:
                 'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
             ],
             'debug': True,
         },
